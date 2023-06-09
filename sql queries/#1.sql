7c2eae0f5d2a76fbf19dfef0885baa551953112e
Alex <asacaquirin17@gmail.com>
2023-06-08 20:53:22 -0400

    done

--- sql queries/#1.sql
+++ sql queries/#1.sql
@@ -40,12 +40,36 @@ FROM data_analyst_jobs;
 -881
 
 --8. How many unique job titles are there for California companies?
-
 SELECT DISTINCT (titLe)
 FROM data_analyst_jobs
 WHERE location ='NJ'
 -- 230
 
+--9 Find the name of each company and its average star rating for all companies that have more than 5000 reviews across all locations. How many companies are there with more that 5000 reviews across all locations?
+SELECT company, review_count
+FROM data_analyst_jobs
+WHERE review_count > 5000
+-- 185
+
+-- 10.Add the code to order the query in #9 from highest to lowest average star rating. Which company with more than 5000 reviews across all locations in the dataset has the highest star rating? What is that rating?
+SELECT company, star_rating,review_count
+FROM data_analyst_jobs
+WHERE review_count > 5000
+-- Kaiser Permanente, 4.19
+
+--11. Find all the job titles that contain the word ‘Analyst’. How many different job titles are there?
+Select title
+From data_analyst_jobs
+Where title LIKE '%Analyst%';
+-- 1636
+
+--12. How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’? What word do these positions have in common?
+
+SELECT title
+FROM data_analyst_jobs
+WHERE title NOT LIKE '%Analyst%'
+AND title NOT LIKE '%Analytics%';
+--Uppr case or lower case
 
 
 
