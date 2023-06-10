04a8dd7ee1e4a0e1874a34df9e2fa2073d8cb565
Alex <asacaquirin17@gmail.com>
2023-06-10 11:00:35 -0400

    done

--- sql queries/#1.sql
+++ sql queries/#1.sql
@@ -8,7 +8,7 @@ from data_analyst_jobs
 Select *
 from data_analyst_jobs
 limit 10
---- XTO Land Data Analyst
+--- ExxonMobil
 
 --3.How many postings are in Tennessee? How many are there in either Tennessee or Kentucky?
 Select location
@@ -27,6 +27,7 @@ AND star_rating > 4
 SELECT *
 FROM data_analyst_jobs
 where review_count BETWEEN 500 AND 1000
+-151
 
 -- 6. Show the average star rating for companies in each state. The output should show the state as state and the average rating for the state as avg_rating. Which state shows the highest average rating?
 SELECT location, AVG(star_rating)
@@ -42,14 +43,16 @@ FROM data_analyst_jobs;
 --8. How many unique job titles are there for California companies?
 SELECT DISTINCT (titLe)
 FROM data_analyst_jobs
-WHERE location ='NJ'
+WHERE location ='CA'
 -- 230
 
 --9 Find the name of each company and its average star rating for all companies that have more than 5000 reviews across all locations. How many companies are there with more that 5000 reviews across all locations?
-SELECT company, review_count
+SELECT (company), AVG (star_rating)
 FROM data_analyst_jobs
 WHERE review_count > 5000
--- 185
+AND company IS NOT NULL
+GROUP BY company;
+-- 40
 
 -- 10.Add the code to order the query in #9 from highest to lowest average star rating. Which company with more than 5000 reviews across all locations in the dataset has the highest star rating? What is that rating?
 SELECT company, star_rating,review_count
@@ -70,8 +73,6 @@ FROM data_analyst_jobs
 WHERE title NOT LIKE '%Analyst%'
 AND title NOT LIKE '%Analytics%';
 --Uppr case or lower case
-
-
 
 
 
